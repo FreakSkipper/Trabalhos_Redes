3eb77658609b46e06747512face476b2d3e00682
Eduardo F. Assis <eduardoffassis@gmail.com>
2019-09-12 10:57:36 -0300

    WIP - Interface, label for terminal output

--- UI/aplicacao.py
+++ UI/aplicacao.py
@@ -1,14 +1,14 @@
 import tkinter
 from Net.conexao import ConexaoHttp
-import os                   # system calls
+import platform                   # system calls
 import webbrowser           # usado para abrir no navegador
 from pathlib import Path    # path to workspace
 
-if os.name is ('nt','dos'):
-    OS = "windows"
+if platform.system() is "Windows":
+    OS = "Windows"
     CLEAR = 'cls'
 else:
-    OS = "unix"
+    OS = "Linux"
     CLEAR = 'clear'
 
 class Aplicacao:
@@ -17,14 +17,17 @@ class Aplicacao:
         print("> " + self.saveSite.get())
         # novo objeto de conexão determinado para o URL
         self.novaConexao = ConexaoHttp(self.saveSite.get())
-
+        self.header = ""
         if self.novaConexao.conectar(2.0):  # nova conexão com TimeOut 2 segundos
             if self.novaConexao.getHTML():  # pega Informação do Site
                 self.novaConexao.encerrar()  # encerra conexão
-                self.novaConexao.printHeader()  # printa cabeçalho de conexão
+                self.header = self.novaConexao.printHeader()  # printa cabeçalho de conexão
+                self.labelTerminal.configure(text=self.header)
+                
                 self.novaConexao.criarHTML()  # cria o arquivo HTML
 
                 self.labelResposta.configure(text="Conectado com sucesso!")
+
             else:
                 self.labelResposta.configure(text="Tempo de conexão excedido!")
         else:
@@ -32,13 +35,15 @@ class Aplicacao:
 
     def abrirBrowser(self):
         work_path = Path(__file__).parent.absolute()
-        print(work_path)
+        # print(work_path)
+        # print(platform.system())
+        # print(OS)
 
         aux = len(str(work_path))
         aux_str = str(work_path)
         caracter_stop = 0
         for i in range(aux-1, -1, -1):
-            if OS == "windows":
+            if OS is "Windows":
                 if aux_str[i] == "\\":
                     caracter_stop = i
                     break
@@ -46,7 +51,7 @@ class Aplicacao:
                 if aux_str[i] == "/":
                     caracter_stop = i
                     break
-        # print(aux_str[0:caracter_stop] + "\index.html")
+        # print(str(caracter_stop) + '\n' + aux_str[0:caracter_stop] + "\index.html")
         if OS == "windows":
             webbrowser.open(aux_str[0:caracter_stop] + "\index.html")
         else:
@@ -56,7 +61,7 @@ class Aplicacao:
         # cria uma aplicação tkinter ( provavelmente falta o Frame )
         self.tela = tkinter.Tk()
         self.tela.title(nome)  # titulo
-        self.tela.geometry(""+str(largura)+"x"+str(altura))  # dimensão
+        self.tela.geometry(str(largura)+"x"+str(altura))  # dimensão
 
         # Label UnB
         self.labelUnb = tkinter.Label(self.tela, text="UnB", fg="blue", font=("Arial", 20, "bold"))
@@ -78,7 +83,7 @@ class Aplicacao:
         self.entrySite.grid(column=0, row=3, padx=10)
 
         # Botão para Iniciar Entrada ( leva para o método entrarSite() )
-        self.buttonEntrar = tkinter.Button(self.tela, text="Entrar", command=self.entrarSite)
+        self.buttonEntrar = tkinter.Button(self.tela, text="Conectar", command=self.entrarSite)
         self.buttonEntrar.grid(column=1, row=3, padx=10)
 
         # Label Saída HTML
@@ -93,5 +98,8 @@ class Aplicacao:
         self.buttonNavegador = tkinter.Button(self.tela, text="Abrir no Navegador", command=self.abrirBrowser)
         self.buttonNavegador.grid(column=1, row=6, padx=10, sticky=tkinter.E)
 
+        # Label saída terminal
+        self.labelTerminal = tkinter.Label(self.tela, text="Label terminal", borderwidth=2, relief=tkinter.GROOVE, width=60, height=30)
+        self.labelTerminal.grid(column=2, row=2)
         # roda loop de apresentação
         self.tela.mainloop()
